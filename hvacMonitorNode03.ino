d9c3fff3f95e3143b2ae7058f56ba8042e3ef9a7
structure7 <kemperm@usa.net>
2016-05-15 16:18:12 -0700

    Heartbeat and high temp alarm

--- hvacMonitorNode03.ino
+++ hvacMonitorNode03.ino
@@ -4,7 +4,7 @@
 #include <BlynkSimpleEsp8266.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <TimeLib.h>  // Remove?
+#include <TimeLib.h>
 #define ONE_WIRE_BUS 2
 
 OneWire oneWire(ONE_WIRE_BUS);
@@ -12,11 +12,14 @@ DallasTemperature sensors(&oneWire);
 
 DeviceAddress ds18b20lk = { 0x28, 0xEE, 0x27, 0x16, 0x01, 0x16, 0x02, 0xED }; // LK
 
-char auth[] = "fromBlynkApp *04";
+float tempLK; // Room temp
+int tempLKhighAlarm = 200;
+
+char auth[] = "fromBlynkApp";
 
 SimpleTimer timer;
 
-WidgetLED led1(V9);
+WidgetLED led1(V9); // Heartbeat LED
 
 void setup()
 {
@@ -29,14 +32,67 @@ void setup()
   sensors.setResolution(ds18b20lk, 10);
 
   timer.setInterval(2000L, sendTemps); // Temperature sensor polling interval
-  timer.setInterval(5000L, sendHeartbeat);
+  timer.setInterval(5000L, heartbeatOn);
+
+  // CODE HERE TO SYNC BACK FROM APP SELECTION?
+}
+
+void heartbeatOn()  // Blinks a virtual LED in the Blynk app to show the ESP is live and reporting.
+{
+  led1.on();
+  timer.setTimeout(2500L, heartbeatOff);
+}
+
+void heartbeatOff()
+{
+  led1.off();  // The OFF portion of the LED heartbeat indicator in the Blynk app
+}
+
+// Input from Blynk app menu to select room temperature that triggers alarm
+BLYNK_WRITE(V21) {
+  switch (param.asInt())
+  {
+    case 1: { // Alarm Off
+        tempLKhighAlarm = 200;
+        break;
+      }
+    case 2: { // 80F Alarm
+        tempLKhighAlarm = 80;
+        break;
+      }
+    case 3: { // 81F Alarm
+        tempLKhighAlarm = 81;
+        break;
+      }
+    case 4: { // 82F Alarm;
+        tempLKhighAlarm = 82;
+        break;
+      }
+    case 5: { // 83F Alarm
+        tempLKhighAlarm = 83;
+        break;
+      }
+    case 6: { // 84F Alarm
+        tempLKhighAlarm = 84;
+        break;
+      }
+    default: {
+        Serial.println("Unknown item selected");
+      }
+  }
+}
+
+void notifyAndOff()
+{
+  Blynk.notify(String("LK's room is ") + tempLK + "°F. Alarm disabled until reset."); // Send notification.
+  Blynk.virtualWrite(V21, 1); // Rather than fancy timing, just disable alarm until reset.
 }
 
 void sendTemps()
 {
   sensors.requestTemperatures(); // Polls the sensors
 
-  float tempLK = sensors.getTempF(ds18b20lk);
+  tempLK = sensors.getTempF(ds18b20lk); // Gets first probe on wire in lieu of by address
 
   if (tempLK > 0)
   {
@@ -46,12 +102,11 @@ void sendTemps()
   {
     Blynk.virtualWrite(6, "ERR");
   }
-    led1.off();
-}
 
-void sendHeartbeat()
-{
-  led1.on();
+  if (tempLK >= tempLKhighAlarm)
+  {
+    notifyAndOff();
+  }
 }
 
 void loop()
